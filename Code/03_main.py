7b9dd8025906de45f19e0de074b9d5329bac63fb
Christian E <chr.espinosa@gmail.com>
2021-11-08 15:21:55 +0100

    Modificar main para bandwidth entre 0.5 i 50 i theta

--- Code/03_main.py
+++ Code/03_main.py
@@ -1,12 +1,72 @@
+#%%
 import pyedflib
 import numpy as np
 import pyarrow as pa
 import pyarrow.parquet as pq
 import os, sys
 import lib.Format_edf_to_paquet as fra
+import matplotlib.pyplot as plt
 
-file_name = os.getcwd() + "/Data/chb01_01.edf"
-path_parquet = os.getcwd() + "/Data/ex.parquet"
+from scipy.signal import freqz
+from scipy.signal import butter, lfilter
+import timeit
+
+
+print("Start")
+start = timeit.default_timer()
+
+name = "chb01_01"
+file_name = os.path.abspath(os.getcwd() + "/Data/edf/" + name + ".edf")
+path_parquet = os.path.abspath(os.getcwd() + "/Data/parquet/" + name + ".parquet")
 edf_f = pyedflib.EdfReader(file_name)
-sig, val = fra.edf_to_numpy(edf_f)
-fra.edf_to_parquet(edf_f, path_parquet)
+df, dic = fra.Create_parquet_from_edf(edf_f, path_parquet)
+print(dic)
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
+print("Overall Plot")
+start = timeit.default_timer()
+
+plt.figure(1)
+plt.plot(dic["FP1-F7"])
+plt.draw()
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
+print("0.5 to 50 Plot")
+start = timeit.default_timer()
+
+fs = 256
+lowcut = 0.5
+highcut = 50
+
+y = fra.butter_bandpass_filter(dic["FP1-F7"], lowcut, highcut, fs, order=6)
+plt.plot(y, label='Filtered signal')
+plt.xlabel('time (seconds)')
+plt.grid(True)
+plt.axis('tight')
+plt.legend(loc='upper left')
+plt.draw()
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
+print("Theta Plot")
+start = timeit.default_timer()
+
+plt.figure(2)
+t_lowcut = 4
+t_highcut = 7
+
+y = fra.butter_bandpass_filter(y, t_lowcut, t_highcut, fs, order=6)
+x = fra.butter_bandpass_filter(dic["FP1-F7"], t_lowcut, t_highcut, fs, order=6)
+plt.plot(x, label='From original signal')
+plt.plot(y, label='From previous filter')
+plt.xlabel('time (seconds)')
+plt.grid(True)
+plt.axis('tight')
+plt.legend(loc='upper left')
+plt.draw()
+
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
+plt.show()
