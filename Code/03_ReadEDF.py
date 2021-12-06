77c1c77a7a82a2f2b8d893214e00ccfc3bf988aa
Christian-Espinosa <33431104+Christian-Espinosa@users.noreply.github.com>
2021-12-06 13:23:22 +0100

    Environment created, automated labeling yet to be done

--- Code/03_ReadEDF.py
+++ Code/03_ReadEDF.py
@@ -65,50 +65,100 @@ def check_23electrodes(f, name, c):
 
 #%%
 #Define Variables and Read edf
-print("Start")
-start = timeit.default_timer()
+single_execution = True
+if single_execution:
+    name_edf = "chb01_03"
+    summary = "chb01-summary"
+    #Paths
+    file_name = os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir) + "/DataSetTFG/CHB-MIT/chb01/edf" + name_edf + ".edf")
+    path_parquet = os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir) + "/DataSetTFG/CHB-MIT/chb01/parquet" + name_edf + ".parquet")
+    file_summary = os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir) + "/DataSetTFG/CHB-MIT/"+ summary + ".txt")
 
-name_edf = "chb01_03"
-summary = "chb01-summary"
-#Paths
-file_name = os.path.abspath(os.getcwd() + os.chdir("../DataSetTFG") + "/Data/edf/" + name + ".edf")
-file_name = os.path.abspath("D:\UAB\4to\DataSetTFG" + "\CHB-MIT\edf" + name_edf + ".edf")
-path_parquet = os.path.abspath("D:\UAB\4to\DataSetTFG" + "\CHB-MIT\parquet" + name_edf + ".parquet")
-file_summary = os.path.abspath("D:\UAB\4to\DataSetTFG" + "\CHB-MIT"+ summary + ".txt")
+    #file_name = os.path.abspath("D:\UAB\4to\DataSetTFG" + "\CHB-MIT\edf" + name_edf + ".edf")
+    #path_parquet = os.path.abspath("D:\UAB\4to\DataSetTFG" + "\CHB-MIT\parquet" + name_edf + ".parquet")
+    #file_summary = os.path.abspath("D:\UAB\4to\DataSetTFG" + "\CHB-MIT"+ summary + ".txt")
 
-#FILTERING FEATURES
-f_range = 'theta'
-hz = 256
-dic_band_definitions = { 'delta' : [0.5, 4],
-                        'theta' : [4, 8],
-                        'alpha' : [8, 12],
-                        'beta' : [12, 30],
-                        'gamma' : [30, 45],
-                        'maxrange' : [0.5, 50]}
-#23 channels
-channels = ["FP1-F7","F7-T7","T7-P7","P7-O1","FP1-F3","F3-C3","C3-P3","P3-O1","FP2-F4","F4-C4","C4-P4","P4-O2","FP2-F8","F8-T8","T8-P8","P8-O2","FZ-CZ","CZ-PZ","P7-T7","T7-FT9","FT9-FT10","FT10-T8","T8-P8"]
+    #FILTERING FEATURES
+    f_range = 'theta'
+    hz = 256
+    dic_band_definitions = { 'delta' : [0.5, 4],
+                            'theta' : [4, 8],
+                            'alpha' : [8, 12],
+                            'beta' : [12, 30],
+                            'gamma' : [30, 45],
+                            'maxrange' : [0.5, 50]}
+    #23 channels
+    channels = ["FP1-F7","F7-T7","T7-P7","P7-O1","FP1-F3","F3-C3","C3-P3","P3-O1","FP2-F4","F4-C4","C4-P4","P4-O2","FP2-F8","F8-T8","T8-P8","P8-O2","FZ-CZ","CZ-PZ","P7-T7","T7-FT9","FT9-FT10","FT10-T8","T8-P8"]
 
+    #%%
+    print("Start")
+    start = timeit.default_timer()
 
+    edf_f = pyedflib.EdfReader(file_name)
+    _ , dic = fra.Create_parquet_from_edf(edf_f, path_parquet)
 
-edf_f = pyedflib.EdfReader(file_name)
-_ , dic = fra.Create_parquet_from_edf(edf_f, path_parquet)
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)  
 
-stop = timeit.default_timer()
-print('Time: ', stop - start)  
+    #%%
+    Rawplot1Channel(dic)
 
-#%%
-Rawplot1Channel(dic)
+    #%%
+    #Define max BandWidth and Theta
+    dic = setBandwidth(dic, dic_band_definitions['maxrange'], hz)
+    dic = setBandwidth(dic, dic_band_definitions[f_range], hz)
 
-#%%
-#Define max BandWidth and Theta
-dic = setBandwidth(dic, dic_band_definitions['maxrange'], hz)
-dic = setBandwidth(dic, dic_band_definitions[f_range], hz)
+    # %%
+    check_23electrodes(file_summary, summary, channels)
+    dic = fra.setLabels(dic, file_summary)
+    if dic == None:
+        print("Error in labeling!")
 
-# %%
-check_23electrodes(file_summary, summary, channels)
-dic = fra.setLabels(dic, file_summary)
-if dic == None:
-    print("Error in labeling!")
+    fra.saveToParquet(dic)
+    plt.show()
 
-fra.saveToParquet(dic)
-plt.show()
+    
+else:
+    # %%
+    n_subjects = 2
+    f_range = 'theta'
+    hz = 256
+
+    for i in range(n_subjects):
+        subj = "chb{:02.0f}".format(i)
+        summary = subj + "-summary"
+        elem = os.listdir(os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir) + "/DataSetTFG/CHB-MIT/" + subj + "/edf"))
+        for j in range(len(elem)):
+            if elem[j].name = acaba en ".edf":
+            name_edf = subj + "_chb{:02.0f}".format(i)
+            file_name = os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir) + "/DataSetTFG/CHB-MIT/" + subj + "/edf" + name_edf + ".edf")
+            path_parquet = os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir) + "/DataSetTFG/CHB-MIT/chb01/parquet" + name_edf + ".parquet")
+            file_summary = os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir) + "/DataSetTFG/CHB-MIT/" + subj + summary + ".txt")
+            dic_band_definitions = { 'delta' : [0.5, 4],
+                                    'theta' : [4, 8],
+                                    'alpha' : [8, 12],
+                                    'beta' : [12, 30],
+                                    'gamma' : [30, 45],
+                                    'maxrange' : [0.5, 50]}
+            channels = ["FP1-F7","F7-T7","T7-P7","P7-O1","FP1-F3","F3-C3","C3-P3","P3-O1","FP2-F4","F4-C4","C4-P4","P4-O2","FP2-F8","F8-T8","T8-P8","P8-O2","FZ-CZ","CZ-PZ","P7-T7","T7-FT9","FT9-FT10","FT10-T8","T8-P8"]
+
+    #%%
+    print("Start")
+    start = timeit.default_timer()
+
+    edf_f = pyedflib.EdfReader(file_name)
+    _ , dic = fra.Create_parquet_from_edf(edf_f, path_parquet)
+
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
+
+    Rawplot1Channel(dic)
+    dic = setBandwidth(dic, dic_band_definitions['maxrange'], hz)
+    dic = setBandwidth(dic, dic_band_definitions[f_range], hz)
+    check_23electrodes(file_summary, summary, channels)
+    dic = fra.setLabels(dic, file_summary)
+    if dic == None:
+        print("Error in labeling!")
+
+    fra.saveToParquet(dic)
+    plt.show()
