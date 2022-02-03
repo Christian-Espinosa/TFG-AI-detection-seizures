82dcd5e83e31edd3ed03be344cece97c901726ea
Christian E <chr.espinosa@gmail.com>
2022-02-03 18:35:57 +0100

    still changing the report

--- Code/04_MExecution.py
+++ Code/04_MExecution.py
@@ -30,63 +30,10 @@ import statistics as stat
 
 import timeit
 
-import ctypes
-import win32api
-import win32security
 
 start = timeit.default_timer()
 
-def suspend(hibernate=False):
-    """Puts Windows to Suspend/Sleep/Standby or Hibernate.
 
-    Parameters
-    ----------
-    hibernate: bool, default False
-        If False (default), system will enter Suspend/Sleep/Standby state.
-        If True, system will Hibernate, but only if Hibernate is enabled in the
-        system settings. If it's not, system will Sleep.
-
-    Example:
-    --------
-    >>> suspend()
-    """
-    # Enable the SeShutdown privilege (which must be present in your
-    # token in the first place)
-    priv_flags = (win32security.TOKEN_ADJUST_PRIVILEGES |
-                  win32security.TOKEN_QUERY)
-    hToken = win32security.OpenProcessToken(
-        win32api.GetCurrentProcess(),
-        priv_flags
-    )
-    priv_id = win32security.LookupPrivilegeValue(
-       None,
-       win32security.SE_SHUTDOWN_NAME
-    )
-    old_privs = win32security.AdjustTokenPrivileges(
-        hToken,
-        0,
-        [(priv_id, win32security.SE_PRIVILEGE_ENABLED)]
-    )
-
-    if (win32api.GetPwrCapabilities()['HiberFilePresent'] == False and
-        hibernate == True):
-            import warnings
-            warnings.warn("Hibernate isn't available. Suspending.")
-    try:
-        ctypes.windll.powrprof.SetSuspendState(not hibernate, True, False)
-    except:
-        # True=> Standby; False=> Hibernate
-        # https://msdn.microsoft.com/pt-br/library/windows/desktop/aa373206(v=vs.85).aspx
-        # says the second parameter has no effect.
-#        ctypes.windll.kernel32.SetSystemPowerState(not hibernate, True)
-        win32api.SetSystemPowerState(not hibernate, True)
-
-    # Restore previous privileges
-    win32security.AdjustTokenPrivileges(
-        hToken,
-        0,
-        old_privs
-    )
 
 #-----------Configure Data Set-----------
 path_model = os.path.abspath(os.path.join(os.getcwd(), os.pardir) + "/DataSetTFG/CHB-MIT/trys")
@@ -104,7 +51,7 @@ outputmodule_params['n_classes']=2
 #CUDA
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
-device = torch.device("cpu")
+#device = torch.device("cpu")
 torch.backends.cudnn.benchmark = True
 
 # Parameters
@@ -211,4 +158,4 @@ else:
         
 stop = timeit.default_timer()
 print('Time: ', stop - start)
-#suspend(True)
+#dat.suspend(True)
--- Code/library/Dataset_Functions.py
+++ Code/library/Dataset_Functions.py
@@ -5,6 +5,9 @@ import torch
 import pandas as pd
 import pyarrow.parquet as pq
 from sklearn import preprocessing
+import ctypes
+import win32api
+import win32security
 
 def CheckModel(model, device):
 
@@ -238,3 +241,55 @@ def test_model(device, model, test_dataloader):
     y_pred_probs = np.round(y_pred_probs, 2)
 
     return y_true, y_pred, y_pred_probs
+
+def suspend(hibernate=False):
+    """Puts Windows to Suspend/Sleep/Standby or Hibernate.
+
+    Parameters
+    ----------
+    hibernate: bool, default False
+        If False (default), system will enter Suspend/Sleep/Standby state.
+        If True, system will Hibernate, but only if Hibernate is enabled in the
+        system settings. If it's not, system will Sleep.
+
+    Example:
+    --------
+    >>> suspend()
+    """
+    # Enable the SeShutdown privilege (which must be present in your
+    # token in the first place)
+    priv_flags = (win32security.TOKEN_ADJUST_PRIVILEGES |
+                  win32security.TOKEN_QUERY)
+    hToken = win32security.OpenProcessToken(
+        win32api.GetCurrentProcess(),
+        priv_flags
+    )
+    priv_id = win32security.LookupPrivilegeValue(
+       None,
+       win32security.SE_SHUTDOWN_NAME
+    )
+    old_privs = win32security.AdjustTokenPrivileges(
+        hToken,
+        0,
+        [(priv_id, win32security.SE_PRIVILEGE_ENABLED)]
+    )
+
+    if (win32api.GetPwrCapabilities()['HiberFilePresent'] == False and
+        hibernate == True):
+            import warnings
+            warnings.warn("Hibernate isn't available. Suspending.")
+    try:
+        ctypes.windll.powrprof.SetSuspendState(not hibernate, True, False)
+    except:
+        # True=> Standby; False=> Hibernate
+        # https://msdn.microsoft.com/pt-br/library/windows/desktop/aa373206(v=vs.85).aspx
+        # says the second parameter has no effect.
+#        ctypes.windll.kernel32.SetSystemPowerState(not hibernate, True)
+        win32api.SetSystemPowerState(not hibernate, True)
+
+    # Restore previous privileges
+    win32security.AdjustTokenPrivileges(
+        hToken,
+        0,
+        old_privs
+    )
